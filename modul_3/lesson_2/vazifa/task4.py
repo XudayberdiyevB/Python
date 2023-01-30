8d1f849f762ce7274f64a8f08c50788ed92a88db
bunyod_hacker <naimov.bunyod2021@gmail.com>
2023-01-30 12:31:40 +0500

    new level 6

--- modul_3/lesson_2/vazifa/task4.py
+++ modul_3/lesson_2/vazifa/task4.py
@@ -6,10 +6,10 @@ only_even_parameters nomli dekorator yarating, bunda quyidagi funksiyallarda ish
 def only_even_parameters(func):
     def only_even(*args):
         for i in args:
-            if i % 2 == 1:
-                return f"Please add only even numbers!"
-            else:
+            if i % 2 == 0:
                 return func(*args)
+            else:
+                return f"Please add only even numbers!"
 
     return only_even
 
@@ -25,4 +25,4 @@ def multiply(a, b, c, d):
 
 
 print(add(2, 8))
-print(multiply(6, 4, 2, 2))
+print(multiply(6, 3, 2, 2))
